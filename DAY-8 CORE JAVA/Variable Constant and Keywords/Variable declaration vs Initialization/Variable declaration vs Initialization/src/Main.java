05337e6ed9e711a965168ed12dcbe8d6353aa3cb
Dharmendra Kumar <118713094+dharmendra9847@users.noreply.github.com>
2026-01-04 19:26:15 +0530

    Variables in Java

---
+++ DAY-8 CORE JAVA/Variable Constant and Keywords/Variable declaration vs Initialization/Variable declaration vs Initialization/src/Main.java
@@ -0,0 +1,25 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        /* 1st approach to declare and initialize the variables*/
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
+        System.out.println("Sum is: " + sum);
+
+        /* 2nd approach to declare and initialize the variables*/
+        int p, q, r, s;
+        p = 20;
+        q = 30;
+        s = 51;
+        r = p+q-s;
+        System.out.println("R is: " + r);
+
+        /* 3rd approach to declare and initialize the variables*/
+        int x = 20, y = 45, z = 65;
+        int result = x + y + z;
+        System.out.println("Here sum of XYZ is: " + result);
+    }
+}
