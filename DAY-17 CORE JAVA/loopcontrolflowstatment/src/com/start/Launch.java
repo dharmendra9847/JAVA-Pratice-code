0b958530415ef1f35bf0a91e097c66fbed015df8
Dharmendra Kumar <118713094+dharmendra9847@users.noreply.github.com>
2026-01-04 19:31:40 +0530

    loop control flow statment

---
+++ DAY-17 CORE JAVA/loopcontrolflowstatment/src/com/start/Launch.java
@@ -0,0 +1,29 @@
+package com.start;
+
+public class Launch {
+
+	public static void main(String[] args) {
+		// WHILE LOOP
+		int i = 1;
+		while(i <= 10) {
+			System.out.println(i);
+			i++;
+		}
+		
+		int a = 20;
+		while(a > 9) {
+			System.out.println(a);
+			a--;
+		}
+		
+		
+		// DO WHILE LOOP
+		int j = 100;
+		do{
+			System.out.println(j);
+			j++;
+		}while(j <= 110);
+
+	}
+
+}
